42bca2bacddad413f0464fca891c4480f566598e
Yacira Nicol.C <91109697+YaciraUTEC@users.noreply.github.com>
2024-06-03 19:31:36 -0500

    Add files via upload

---
+++ haversine.py
@@ -0,0 +1,16 @@
+from math import radians, cos, sin, sqrt, atan2
+
+def haversine_distance(coord1, coord2):
+    R = 6371.0  # Radio de la Tierra en kilómetros
+    
+    lat1, lon1 = radians(coord1[0]), radians(coord1[1])
+    lat2, lon2 = radians(coord2[0]), radians(coord2[1])
+    
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    
+    distance = R * c
+    return distance
